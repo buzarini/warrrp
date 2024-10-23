8d24eb96e096ab57aa2222b174cac61b24ff6858
buzarini <43939914+buzarini@users.noreply.github.com>
2024-10-24 02:52:14 +0300

    Update warrrp.py

--- warrrp.py
+++ warrrp.py
@@ -4,12 +4,41 @@ import base64
 import pytz
 import datetime
 import subprocess
+import ipaddress
+
+warp_cidr = [
+        '162.159.192.0/24',
+        '162.159.195.0/24',
+        '188.114.96.0/24',
+        '188.114.97.0/24'
+    ]
 
 # Constants for paths
 SCRIPT_DIR = os.path.dirname(__file__)
 WARP_SERVER_SCANNER_PATH = os.path.join(SCRIPT_DIR, 'bin', 'warp')
 SERVER_SCAN_RESULTS_PATH = os.path.join(SCRIPT_DIR, 'result.csv')
 CONFIG_FILE_PATH = os.path.join(SCRIPT_DIR, 'config')
+IP_FILE_PATH = os.path.join(SCRIPT_DIR, 'ip.txt')
+
+def create_ips():
+    c = 0
+    total_ips = sum(len(list(ipaddress.IPv4Network(cidr))) for cidr in warp_cidr)
+
+    with open(IP_FILE_PATH, 'w') as file:
+        for cidr in warp_cidr:
+            ip_addresses = list(ipaddress.IPv4Network(cidr))
+            for addr in ip_addresses:
+                c += 1
+                file.write(str(addr))
+                if c != total_ips:
+                    file.write('\n')
+
+if os.path.exists(IP_FILE_PATH):
+    print("ip.txt exist.")
+else:
+    print('Creating ip.txt File.')
+    create_ips()
+    print('ip.txt File Created Successfully!')
 
 def get_repository_name():
     """Returns the uppercase name of the repository."""
@@ -54,30 +83,30 @@ def base64_encode(data):
     return base64.b64encode(data.encode('utf-8')).decode('utf-8')
 
 def get_last_update_time():
-    """Returns the last update time of the result CSV file in Tehran time."""
+    """Returns the last update time of the result CSV file in Moscow time."""
     try:
         creation_time = os.path.getctime(SERVER_SCAN_RESULTS_PATH)
     except OSError as e:
         print(f"Error accessing the result CSV file: {e}")
         return None
 
-    tehran_tz = pytz.timezone('Asia/Tehran')
-    local_time = datetime.datetime.fromtimestamp(creation_time, tehran_tz)
-    return local_time.strftime("%Y-%m-%d %H:%M") + " Tehran, Iran Time"
+    moscow_tz = pytz.timezone('Europe/Moscow')
+    local_time = datetime.datetime.fromtimestamp(creation_time, moscow_tz)
+    return local_time.strftime("%d-%m-%Y %H:%M") + " Moscow, Russia Time"
 
 def generate_warp_config(top_servers, last_update_time):
     """Generates and writes the Warp configuration based on the top servers and last update time."""
     plus_key = os.getenv('PLUS_KEY')
 
     warp_config = (
-        f"warp://{top_servers[0]}?ifp=20-35&ifps=5-35&ifpd=5-35&ifpm=m4#IR&&detour=warp://{top_servers[1]}#DE"
+        f"warp://{top_servers[0]}?ifp=20-35&ifps=5-35&ifpd=5-35&ifpm=m4#Warp&&detour=warp://{top_servers[1]}#Warp-IN-Warp"
     )
 
     warp_hiddify_config = (
         f"#profile-title: base64:{base64_encode(get_repository_name())}\n"
         f"#profile-update-interval: 1\n"
         f"#subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=2546249531\n"
-        f"#profile-web-page-url: https://github.com/rzrhmrd/warrrp\n"
+        f"#profile-web-page-url: https://github.com/buzarini/warrrp\n"
         f"#last-update: {last_update_time}\n"
         f"{warp_config}"
     )
@@ -89,11 +118,13 @@ def generate_warp_config(top_servers, last_update_time):
         print(f"Error writing to configuration file: {e}")
 
 def clean_up():
-    """Cleans up by removing the result CSV file."""
+    """Cleans up by removing the result CSV and TXT files."""
     try:
         os.remove(SERVER_SCAN_RESULTS_PATH)
+        os.remove(IP_FILE_PATH)
     except OSError as e:
         print(f"Error removing file {SERVER_SCAN_RESULTS_PATH}: {e}")
+        print(f"Error removing file {IP_FILE_PATH}: {e}")
 
 def main():
     """Main function to run the warp server scanner and generate the config."""
